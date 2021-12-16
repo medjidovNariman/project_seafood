eb35e06d797211bfb07af2da071c716cfc57e9bf
nariman <nariman.miedzhidov@mail.ru>
2021-12-16 08:33:55 +0300

    product

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@ $(function(){
     nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
     vertical: true,
   });
+
+  
+  $('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/product_prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product_next.svg" alt=""></button>',
+    asNavFor: '.product__content',
+    vertical: true,
+  });
+  $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false
+  });
+
+
 });
